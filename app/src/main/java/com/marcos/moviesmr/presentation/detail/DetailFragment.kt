43756e6896bd73f11ab7c8cc765b614522e0bddb
Marcos <mramosmarcos14@gmail.com>
2022-12-22 17:20:02 -0300

    Implement detail fragment

--- app/src/main/java/com/marcos/moviesmr/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/marcos/moviesmr/presentation/detail/DetailFragment.kt
@@ -6,13 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.marcos.moviesmr.R
+import com.marcos.moviesmr.databinding.FragmentDetailBinding
+import com.marcos.moviesmr.framework.imageLoader.ImageLoader
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class DetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailBinding
+    private val detailAdapter: DetailAdapter by lazy { DetailAdapter(imageLoader) }
+
+    @Inject
+    lateinit var imageLoader: ImageLoader
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ) = FragmentDetailBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initDetailAdapter()
+    }
+
+    private fun initDetailAdapter() {
+        with(binding.recyclerMoviesDetail) {
+            setHasFixedSize(true)
+            adapter = detailAdapter
+        }
     }
 }
