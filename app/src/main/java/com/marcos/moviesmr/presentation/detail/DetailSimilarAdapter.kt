cabd90d3f1fdeee62c6f196326c0720ee10d52ef
Marcos <mramosmarcos14@gmail.com>
2023-01-13 20:59:15 -0300

    Instantiate new adapter for details

--- app/src/main/java/com/marcos/moviesmr/presentation/detail/DetailSimilarAdapter.kt
+++ app/src/main/java/com/marcos/moviesmr/presentation/detail/DetailSimilarAdapter.kt
@@ -2,25 +2,25 @@ package com.marcos.moviesmr.presentation.detail
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.marcos.moviesmr.core.domain.model.Movie
 import com.marcos.moviesmr.databinding.ItemMoviesBinding
 import com.marcos.moviesmr.framework.imageLoader.ImageLoader
+import com.marcos.moviesmr.presentation.detail.DetailSimilarAdapter.DetailSimilarViewHolder
 
 class DetailSimilarAdapter(
-    private val detailSimilarList: List<Movie>,
     private val imageLoader: ImageLoader
-) : RecyclerView.Adapter<DetailSimilarAdapter.DetailSimilarViewHolder>() {
+) : ListAdapter<Movie, DetailSimilarViewHolder>(diffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailSimilarViewHolder {
         return DetailSimilarViewHolder.create(parent, imageLoader)
     }
 
     override fun onBindViewHolder(holder: DetailSimilarViewHolder, position: Int) {
-        holder.bind(detailSimilarList[position])
+        holder.bind(getItem(position))
     }
-
-    override fun getItemCount() = detailSimilarList.size
 
     class DetailSimilarViewHolder(
         itemMoviesBinding: ItemMoviesBinding,
@@ -45,6 +45,24 @@ class DetailSimilarAdapter(
                 val itemMoviesBinding = ItemMoviesBinding
                     .inflate(LayoutInflater.from(parent.context), parent, false)
                 return DetailSimilarViewHolder(itemMoviesBinding, imageLoader)
+            }
+        }
+    }
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<Movie>() {
+            override fun areItemsTheSame(
+                oldItem: Movie,
+                newItem: Movie
+            ) : Boolean {
+                return oldItem.title == newItem.title
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Movie,
+                newItem: Movie
+            ) : Boolean {
+                return oldItem == newItem
             }
         }
     }
