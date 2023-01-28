f084104764bcd6c4c90ade57638ef488b01c71cc
Marcos <mramosmarcos14@gmail.com>
2023-01-28 11:01:59 -0300

    Create adapter to list searched movies

--- app/src/main/java/com/marcos/moviesmr/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/marcos/moviesmr/presentation/search/SearchFragment.kt
@@ -1,16 +1,28 @@
 package com.marcos.moviesmr.presentation.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.marcos.moviesmr.R
+import androidx.fragment.app.Fragment
+import com.marcos.moviesmr.core.domain.model.Movie
 import com.marcos.moviesmr.databinding.FragmentSearchBinding
+import com.marcos.moviesmr.framework.imageLoader.ImageLoader
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SearchFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchBinding
+    private val searchAdapter: SearchAdapter by lazy {
+        val adapter = SearchAdapter(imageLoader)
+        binding.recyclerSearch.adapter = adapter
+        return@lazy adapter
+    }
+
+    @Inject
+    lateinit var imageLoader: ImageLoader
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,4 +34,16 @@ class SearchFragment : Fragment() {
     ).apply {
         binding = this
     }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        searchAdapter.submitList(
+            listOf(
+               Movie(24428, "The Avengers", "2012-04-25", "/hbn46fQaRmlpBuUrEiFqv0GDL6Y.jpg", 8503, 7.353212),
+               Movie(24428, "The Avengers", "2012-04-25", "/hbn46fQaRmlpBuUrEiFqv0GDL6Y.jpg", 8503, 7.353212),
+               Movie(24428, "The Avengers", "2012-04-25", "/hbn46fQaRmlpBuUrEiFqv0GDL6Y.jpg", 8503, 7.353212)
+            )
+        )
+    }
 }
