748d693055f085169305d9ca0551aee3c9316701
Marcos <mramosmarcos14@gmail.com>
2023-01-28 11:01:59 -0300

    Create adapter to list searched movies

---
+++ app/src/main/java/com/marcos/moviesmr/presentation/search/SearchAdapter.kt
@@ -0,0 +1,69 @@
+package com.marcos.moviesmr.presentation.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.marcos.moviesmr.core.domain.model.Movie
+import com.marcos.moviesmr.databinding.ItemMoviesBinding
+import com.marcos.moviesmr.framework.imageLoader.ImageLoader
+import com.marcos.moviesmr.presentation.search.SearchAdapter.SearchViewHolder
+
+class SearchAdapter(
+    private val imageLoader: ImageLoader
+) : ListAdapter<Movie, SearchViewHolder>(diffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : SearchViewHolder {
+        return SearchViewHolder.create(parent, imageLoader)
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class SearchViewHolder(
+        itemMoviesBinding: ItemMoviesBinding,
+        private val imageLoader: ImageLoader
+    ) : RecyclerView.ViewHolder(itemMoviesBinding.root) {
+
+        private val textName = itemMoviesBinding.textName
+        private val textYear = itemMoviesBinding.textYear
+        private val imageMovies = itemMoviesBinding.imageMovies
+
+        fun bind(movie: Movie) {
+            textName.text = movie.title
+            textYear.text = movie.year
+            imageLoader.load(imageMovies, movie.imageUrl)
+        }
+
+        companion object {
+            fun create(
+                parent: ViewGroup,
+                imageLoader: ImageLoader
+            ) : SearchViewHolder {
+                val itemMoviesBinding = ItemMoviesBinding
+                    .inflate(LayoutInflater.from(parent.context), parent, false)
+                return SearchViewHolder(itemMoviesBinding, imageLoader)
+            }
+        }
+    }
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<Movie>() {
+            override fun areItemsTheSame(
+                oldItem: Movie,
+                newItem: Movie
+            ) : Boolean {
+                return oldItem.title == newItem.title
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Movie,
+                newItem: Movie
+            ) : Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
