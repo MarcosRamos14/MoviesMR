31bd3766b8f55eac536ffe3d48351f69a75d4109
Marcos <mramosmarcos14@gmail.com>
2023-02-13 15:07:05 -0300

    Configure Hilt for test class

--- app/src/androidTest/java/com/marcos/moviesmr/presentation/home/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/marcos/moviesmr/presentation/home/HomeFragmentTest.kt
@@ -1,17 +1,24 @@
 package com.marcos.moviesmr.presentation.home
 
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.marcos.moviesmr.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@HiltAndroidTest
 @RunWith(AndroidJUnit4::class)
 class HomeFragmentTest {
 
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
     @Before
     fun setUp() {
-        launchFragmentInContainer<HomeFragment>()
+        launchFragmentInHiltContainer<HomeFragment>()
     }
 
     @Test
