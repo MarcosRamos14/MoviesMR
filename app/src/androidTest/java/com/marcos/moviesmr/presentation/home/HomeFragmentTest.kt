ba6563bc1707e40bf01e442391d825187bc96784
Marcos <mramosmarcos14@gmail.com>
2023-02-13 16:23:41 -0300

    Replaces baseUrl to make request to local host

--- app/src/androidTest/java/com/marcos/moviesmr/presentation/home/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/marcos/moviesmr/presentation/home/HomeFragmentTest.kt
@@ -1,15 +1,23 @@
 package com.marcos.moviesmr.presentation.home
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.marcos.moviesmr.R
+import com.marcos.moviesmr.framework.di.BaseUrlModule
 import com.marcos.moviesmr.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @HiltAndroidTest
+@UninstallModules(BaseUrlModule::class)
 @RunWith(AndroidJUnit4::class)
 class HomeFragmentTest {
 
@@ -23,6 +31,10 @@ class HomeFragmentTest {
 
     @Test
     fun shouldShowPopularMovies_viewIsCreated() {
-
+        onView(
+            withId(R.id.recycler_movies_home)
+        ).check(
+            matches(isDisplayed())
+        )
     }
 }
